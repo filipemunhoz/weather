60e0140ba85f53b450e8091ac79a210808f560c1
Filipe Munhoz <filipemunhoz@gmail.com>
2023-02-25 08:59:56 -0300

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::time::Instant;
-
 use reqwest::Client;
-use serde::{Deserialize};
+use serde::Deserialize;
 use futures::{stream, StreamExt};
 
 const CONCURRENT_REQUESTS: usize = 2;
@@ -38,8 +37,7 @@ async fn main() -> Result<(), reqwest::Error> {
     let start = Instant::now();
 
     let urls = vec!["https://api.openweathermap.org/data/2.5/weather?units=metric&lat=51.509865&lon=-0.118092&appid=71e4be94cf9827a2d5657f6a140ad79e",
-                                    "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=-23.5489&lon=-46.6388&appid=71e4be94cf9827a2d5657f6a140ad79e"];
-
+                                    "https:/api.openweathermap.org/data/2.5/weather?units=metric&lat=-23.5489&lon=-46.6388&appid=71e4be94cf9827a2d5657f6a140ad79e"];
 
     let client = Client::new();
     let bodies = stream::iter(urls)
@@ -55,37 +53,39 @@ async fn main() -> Result<(), reqwest::Error> {
     bodies
         .for_each(|b| async {
             match b {
-                Ok(b) => { 
-                    let weather: Weather = serde_json::from_slice(&b).unwrap();                    
-                    let temperature = weather.main.temp.round() as i32;
-                    let temp_min = weather.main.temp_min.round() as i32;
-                    let temp_max = weather.main.temp_max.round() as i32;
-                    
-                    let wind_degrees = weather.wind.deg;
-                    let wind_speed = weather.wind.speed.round() as i32;
-
-                    let weather_condition = &weather.weather.first().ok_or("").unwrap().main;
-
-                    println!("{:<12} {:>2}° {:>2}{} {:>2}{}  {}  {}{}  {}",
-                        weather.name, 
-                        temperature, 
-                        temp_min, '⬇', 
-                        temp_max, '⬆', 
-                        emoji_face(temperature),
-                        wind_speed,
-                        emoji_wind_direction(wind_degrees),
-                        emoji_weather_condition(weather_condition),                 
-                    );
+                Ok(b) => {
+                    print_output(&b);
                 },
-                Err(e) => eprintln!("Got an error: {}", e),
+                Err(_e) => eprintln!("Not connected"),
             }
         })
         .await;
 
     let duration = start.elapsed().as_millis();
 
-    println!("Connected {}ms", duration);
+    println!("Duration {}ms", duration);
     Ok(())
+}
+
+fn print_output(b: &[u8]){
+    let weather: Weather = serde_json::from_slice(&b).unwrap();
+    let temperature = weather.main.temp.round() as i32;
+    let temp_min = weather.main.temp_min.round() as i32;
+    let temp_max = weather.main.temp_max.round() as i32;    
+    let wind_degrees = weather.wind.deg;
+    let wind_speed = weather.wind.speed.round() as i32;
+    let weather_condition = &weather.weather.first().ok_or("").unwrap().main;
+
+    println!("{:<12} {:>2}° {:>2}{} {:>2}{}  {}  {}{}  {}",
+        weather.name, 
+        temperature, 
+        temp_min, '⬇', 
+        temp_max, '⬆', 
+        emoji_face(temperature),
+        wind_speed,
+        emoji_wind_direction(wind_degrees),
+        emoji_weather_condition(weather_condition),
+    );    
 }
 
 fn emoji_face(temperature: i32) -> String {
@@ -98,8 +98,8 @@ fn emoji_face(temperature: i32) -> String {
     }    
 }
 
-fn emoji_wind_direction(d: i32) -> String {
-    match d {
+fn emoji_wind_direction(degree: i32) -> String {
+    match degree {
         0..=10 => format!("{}",'⬇'),
         11..=78 => format!("{}",'↙'),
         79..=123 => format!("{}",'⬅'),
